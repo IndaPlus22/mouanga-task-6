58c9e5975020afbf92adfb6784a9de1c8d2a61cd
salticecream <113557800+salticecream@users.noreply.github.com>
2022-10-17 07:14:01 +0200

    add skyffla
    haskell slander memes are well on their way

--- F1.hs
+++ F1.hs
@@ -1,5 +1,7 @@
+module F1 where
 import Data.Char
 
+-- 1: fibonacci
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
@@ -23,7 +25,7 @@ fib 41 = 165580141
 fib 42 = 267914296
 fib n = fib (n-2) + fib (n-1)
 
-
+-- 2: rovarsprak
 rovarsprak :: String -> String
 rovarsprak "" = ""
 rovarsprak a = if elem (head a) "bcdfghjklmnpqrstvwxz" then head a:'o':head a:rovarsprak (tail a) else head a:rovarsprak (tail a)
@@ -38,24 +40,21 @@ karpsravor a = if elem (head a) "bcdfghjklmnpqrstvwxz" then
 													   else head a : karpsravor (tail a) -- same as above, but now we're also at the end of the list (this part is probably unnecessary)
 			   else head a : karpsravor (tail a) -- ok, this is a vowel
 
--- if head a == a !! 2 && a !! 1 == 'o' then head a: drop 3 (karpsravor a) else head a:karpsravor (tail a) else head a:karpsravor (tail a)
-
-
-sentence :: String -> String
-sentence "" = ""
-sentence a = if isAlpha(head a) == False then ' ' : sentence (tail a) else head a : sentence (tail a)
+-- 3: medellangd
+mening :: String -> String
+mening "" = ""
+mening a = if isAlpha(head a) == False then ' ' : mening (tail a) else head a : mening (tail a)
 
 medellangd :: String -> Double
-medellangd a = (fromIntegral (sum (map length (words (sentence a))) ) :: Double) / fromIntegral (length (words (sentence a))) :: Double
+medellangd a = (fromIntegral (sum (map length (words (mening a))) ) :: Double) / fromIntegral (length (words (mening a))) :: Double
+
+-- 4: skyffla
+
+varannan :: [a] -> [a]
+varannan [] = []
+varannan a = a !! 0 : varannan ( (drop 2) a)
 
 
 skyffla :: [a] -> [a]
 skyffla [] = []
-skyffla a = skyffla' [] 9
-
-skyffla' :: [a] -> Int -> [a]
-skyffla' [] 0 = []
-skyffla' a 0 = a
-skyffla' a b = (head a) : (tail a)
-
--- Först ta huvudet, sedan droppa 2:an, sedan ta allt som inte droppats, lägg till 2:an i slutet, minska räknaren med 1
+skyffla a = (varannan a) ++ (varannan (tail (a) ) )
